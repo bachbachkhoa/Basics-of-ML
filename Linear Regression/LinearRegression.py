23dec34629780d25bde6ce6d7d9aef9d07334f8e
bachbachkhoa <42577469+bachbachkhoa@users.noreply.github.com>
2021-03-18 17:59:29 +0700

    Update LinearRegression.py

--- Linear Regression/LinearRegression.py
+++ Linear Regression/LinearRegression.py
@@ -1,1 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+data = pd.read_excel(r'data.xlsx')
+df = pd.DataFrame(data, columns = ['Height', 'Weight'])
+X, y = np.array([df['Height']]).T, np.array([df['Weight']]).T
+
+one = np.ones((X.shape[0], 1))
+# Building Xbar 
+one = np.ones((X.shape[0], 1))
+Xbar = np.concatenate((one, X), axis = 1)
+
+# Calculating weights of the fitting line 
+A = np.dot(Xbar.T, Xbar)
+b = np.dot(Xbar.T, y)
+w = np.dot(np.linalg.pinv(A), b)
+print('w = ', w)
+# Preparing the fitting line 
+w_0 = w[0][0]
+w_1 = w[1][0]
+x0 = np.linspace(145, 185, 2)
+y0 = w_0 + w_1*x0
+
+# Drawing the fitting line 
+plt.plot(X.T, y.T, 'ro')     # data 
+plt.plot(x0, y0)               # the fitting line
+plt.axis([140, 190, 45, 75])
+plt.xlabel('Height (cm)')
+plt.ylabel('Weight (kg)')
+plt.show()
